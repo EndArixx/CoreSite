edc65993118b3a52115ab035b23b0ed79f4bff26
John Mark Stanley <endarixx@users.noreply.github.com>
2020-05-24 16:03:45 -0400

    Establishes comic hierarchy

--- Core/ccc/admin.py
+++ Core/ccc/admin.py
@@ -4,13 +4,58 @@ from django.forms.models import BaseInlineFormSet, ModelForm
 
 admin.site.register(Page)
 
-class Page_Comic_Inline(admin.StackedInline):
-    ordering = ('PagID__Number',)
-    model = Page_Comic
+class Comic_Series_Inline(admin.StackedInline):
+    ordering = ('SerId__Name',)
+    model = Comic_Series
     extra = 0
-	
+    
 class ComicAdmin(admin.ModelAdmin):
-	ordering = ('Name',)
-	inlines = [Page_Comic_Inline]
-
+    ordering = ('Name',)
+    inlines = [Comic_Series_Inline]
+    
 admin.site.register(Comic,ComicAdmin)
+
+class Series_Season_Inline(admin.StackedInline):
+    ordering = ('SeaId__Name',)
+    model = Series_Season
+    extra = 0
+    
+class SeriesAdmin(admin.ModelAdmin):
+    ordering = ('Name',)
+    inlines = [Series_Season_Inline]
+
+admin.site.register(Series,SeriesAdmin)
+
+class Season_Issue_Inline(admin.StackedInline):
+    ordering = ('IssId__Name',)
+    model = Season_Issue
+    extra = 0
+    
+class SeasonAdmin(admin.ModelAdmin):
+    ordering = ('Name',)
+    inlines = [Season_Issue_Inline]
+
+admin.site.register(Season,SeasonAdmin)
+
+class Issue_Chapter_Inline(admin.StackedInline):
+    ordering = ('ChaId__Name',)
+    model = Issue_Chapter
+    extra = 0
+    
+class IssueAdmin(admin.ModelAdmin):
+    ordering = ('Name',)
+    inlines = [Issue_Chapter_Inline]
+
+admin.site.register(Issue,IssueAdmin)
+
+class Chapter_Page_Inline(admin.StackedInline):
+    ordering = ('PagId__Name',)
+    model = Chapter_Page
+    extra = 0
+    
+class ChapterAdmin(admin.ModelAdmin):
+    ordering = ('Name',)
+    inlines = [Chapter_Page_Inline]
+
+admin.site.register(Chapter,ChapterAdmin)
+
--- Core/ccc/models.py
+++ Core/ccc/models.py
@@ -4,50 +4,173 @@ from datetime import datetime
 
 '''
 IMPORTANT Identifiers/ForeignKeys:
-	--Comic--
+    --Comic--
+    ComId = Comic Id
     SerId = Series Id
     SeaId = Season Id
-    ComID = Comic Id
-    ChaID = Chapter Id
+    IssId = Issue Id
+    ChaId = Chapter Id
     PagId = Page Id
+    PanId = Panel Id
+
+    TagId = Tag Id
+    AutId = Author Id
+    GenId = Genre Id
 '''
 
 '''
 ---Heirarchy:---
+    Comic 
     Series
     Season
-    Comic
+    Issue
     Chapter
     Page
+    Panel
 '''
 
+'''
+---Search info---
+Tag
+Auther
+Genre
+Date
+'''
+
+#########################################################
+#                  Hierarchy of tables                  #
+#########################################################
 class Comic(models.Model):
-    ComID = models.AutoField(primary_key=True)
+    ComId = models.AutoField(primary_key=True)
     Name =  models.CharField(max_length=200)
-    #should this be its own table?
-    Description =  models.CharField(max_length=4000,blank=True, null=True)
-    #should this be its own table?
-    Summary = models.CharField(max_length=4000,blank=True, null=True)
-    #Tag:                       one to many tables
-    #Auther/Artist/Creator:     one to many tables
-    #Genre:                     one to many tables
+    Date = models.DateField()
+    Description =  models.CharField(max_length=8000,blank=True, null=True)
+    Summary = models.CharField(max_length=8000,blank=True, null=True)
+    class Meta:
+        verbose_name = 'Comic'
+        verbose_name_plural = '1) Comics' 
+    def __str__(self):
+        return self.Name
+
+class Series(models.Model):
+    SerId = models.AutoField(primary_key=True)
+    Name =  models.CharField(max_length=200)
+    Description =  models.CharField(max_length=8000,blank=True, null=True)
+    Summary = models.CharField(max_length=8000,blank=True, null=True)
+    Start = models.DateField(null=True)
+    End = models.DateField(null=True)
+    class Meta:
+        verbose_name = 'Series'
+        verbose_name_plural = '2) serieses' 
+    def __str__(self):
+        return self.Name
+
+class Season(models.Model):
+    SeaId = models.AutoField(primary_key=True)
+    Name =  models.CharField(max_length=200)
+    Description =  models.CharField(max_length=8000,blank=True, null=True)
+    Summary = models.CharField(max_length=8000,blank=True, null=True)
+    Start = models.DateField(null=True)
+    End = models.DateField(null=True)
+    class Meta:
+        verbose_name = 'Season'
+        verbose_name_plural = '3) Seasons' 
+    def __str__(self):
+        return self.Name
+
+class Issue(models.Model):
+    IssId = models.AutoField(primary_key=True)
+    Name =  models.CharField(max_length=200)
+    Date = models.DateField(null=True)
+    Description =  models.CharField(max_length=8000,blank=True, null=True)
+    Summary = models.CharField(max_length=8000,blank=True, null=True)
+    class Meta:
+        verbose_name = 'Issue'
+        verbose_name_plural = '4) Issues' 
+    def __str__(self):
+        return self.Name
+
+class Chapter(models.Model):
+    ChaId = models.AutoField(primary_key=True)
+    Name =  models.CharField(max_length=200)
+    Description =  models.CharField(max_length=8000,blank=True, null=True)
+    Summary = models.CharField(max_length=8000,blank=True, null=True)
+    class Meta:
+        verbose_name = 'Chapter'
+        verbose_name_plural = '5) Chapters' 
     def __str__(self):
         return self.Name
 
 class Page(models.Model):
-    PagID = models.AutoField(primary_key=True)
+    PagId = models.AutoField(primary_key=True)
     Name =  models.CharField(max_length=200)
     Number = models.IntegerField(default=0)
     Image = models.ImageField()
     class Meta:
+        verbose_name = 'Page'
+        verbose_name_plural = '6) Pages' 
         unique_together = ('Name', 'Number')
     def __str__(self):
         return self.Name + ' - '+ str(self.Number)
 
-class Page_Comic(models.Model):
-	ComID = models.ForeignKey(Comic, on_delete=models.CASCADE) 
-	PagID =  models.ForeignKey(Page, on_delete=models.CASCADE) 
-	class Meta:
-		unique_together = ('ComID', 'PagID')
-	def __str__(self):
-		return self.ComID.Name + ' - '+ str(self.PagID.Number)
+
+class Panel(models.Model):
+    PanId = models.AutoField(primary_key=True)
+    PagId =  models.ForeignKey(Page, on_delete=models.CASCADE) 
+    Number = models.IntegerField(default=0)
+    Image = models.ImageField()
+    class Meta:
+        verbose_name = 'Panel'
+        verbose_name_plural = '7) Panels' 
+        unique_together = ('PagId', 'Number')
+    def __str__(self):
+        return PagId.Name + ' - ' + str(PagId.Number) + ' (' + str(self.Number) + ')'
+
+
+#########################################################
+#                     Relationships                     #
+#########################################################
+
+class Comic_Series(models.Model):
+    ComId = models.ForeignKey(Comic, on_delete=models.CASCADE) 
+    SerId =  models.ForeignKey(Series, on_delete=models.CASCADE) 
+    class Meta:
+        unique_together = ('ComId', 'SerId')
+    def __str__(self):
+        return self.ComId.Name + ' - '+ str(self.SerId.Number)
+
+class Series_Season(models.Model):
+    SerId =  models.ForeignKey(Series, on_delete=models.CASCADE) 
+    SeaId = models.ForeignKey(Season, on_delete=models.CASCADE) 
+    class Meta:
+        unique_together = ('SerId', 'SeaId')
+    def __str__(self):
+        return self.SerId.Name + ' - '+ str(self.SeaId.Number)
+
+class Season_Issue(models.Model):
+    SeaId =  models.ForeignKey(Season, on_delete=models.CASCADE) 
+    IssId = models.ForeignKey(Issue, on_delete=models.CASCADE) 
+    class Meta:
+        unique_together = ('SeaId', 'IssId')
+    def __str__(self):
+        return self.SeaId.Name + ' - '+ str(self.IssId.Number)
+
+class Issue_Chapter(models.Model):
+    IssId =  models.ForeignKey(Issue, on_delete=models.CASCADE)
+    ChaId = models.ForeignKey(Chapter, on_delete=models.CASCADE) 
+    class Meta:
+        unique_together = ('IssId', 'ChaId')
+    def __str__(self):
+        return self.IssId.Name + ' - '+ str(self.ChaId.Number)
+
+class Chapter_Page(models.Model):
+    ChaId = models.ForeignKey(Chapter, on_delete=models.CASCADE) 
+    PagId =  models.ForeignKey(Page, on_delete=models.CASCADE) 
+    class Meta:
+        unique_together = ('ChaId', 'PagId')
+    def __str__(self):
+        return self.ChaId.Name + ' - '+ str(self.PagId.Number)
+
+#########################################################
+#                     Search Tables                     #
+#########################################################
